93863f2f4d4387094d5865c44196b874714b3257
suyunbae <suybae@ucdavis.edu>
2017-06-29 00:26:07 -0700

    %

--- js/boo.js
+++ js/boo.js
@@ -14,7 +14,7 @@ $http.get(openWeatherURL).success(function(data){
 vm.description= data.weather[0].description;
 vm.speed= (2.237*data.wind.speed).toFixed(1) + " mph";
 vm.name= data.name; 
-vm.humidity = data.main.humidity; + " %"
+vm.humidity = data.main.humidity; + " %";
 vm.temp= data.main.temp;
 vm.fTemp= (vm.temp*(9/5)-459.67).toFixed(1) + "  °F";
 vm.cTemp= (vm.temp-273).toFixed(1) + "  °C";
