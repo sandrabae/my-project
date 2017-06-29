0f719122d24a79d8d6e30042df7c173b4d68aecb
suyunbae <suybae@ucdavis.edu>
2017-06-28 22:29:25 -0700

    s

--- js/boo.js
+++ js/boo.js
@@ -26,59 +26,59 @@ vm.fTemp= (vm.temp*(9/5)-459.67).toFixed(1) + "  (°F)";
 vm.cTemp= (vm.temp-273).toFixed(1) + "  (°C)";
   vm.icon= "http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png";
   
-  switch(vm.description){
-    case 'clear sky':{
-      vm.weatherBackground= {
-        "background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/clearSky.jpg')",
-        "background-size": "cover"
-      };
-      break;
-    }
-       case "broken clouds": {
-          vm.weatherBackground = {"background": "url(' http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/brokenClouds.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-             case "few clouds": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/fewClouds.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-           case "mist": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/mist.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-        case "rain": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/rain.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-            case "scattered clouds": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/scatteredClouds.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-          case "shower rain": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/showerRain.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-          case "snow": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/snow.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-          case "thunderstorm": {
-          vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/Thunder.jpg')",
-             "background-size": "cover" };
-            break;
-          }
-    default:
-      vm.weatherBackground= 
-        {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/sun.jpg')",
-             "background-size": "cover" };
-    break;
+  // switch(vm.description){
+  //   case 'clear sky':{
+  //     vm.weatherBackground= {
+  //       "background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/clearSky.jpg')",
+  //       "background-size": "cover"
+  //     };
+  //     break;
+  //   }
+  //      case "broken clouds": {
+  //         vm.weatherBackground = {"background": "url(' http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/brokenClouds.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //            case "few clouds": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/fewClouds.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //          case "mist": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/mist.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //       case "rain": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/rain.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //           case "scattered clouds": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/scatteredClouds.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //         case "shower rain": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/showerRain.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //         case "snow": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/snow.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //         case "thunderstorm": {
+  //         vm.weatherBackground = {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/Thunder.jpg')",
+  //            "background-size": "cover" };
+  //           break;
+  //         }
+  //   default:
+  //     vm.weatherBackground= 
+  //       {"background": "url('http://codingtutorials360.com/img/FreeCodeCamp/OpenWeather/sun.jpg')",
+  //            "background-size": "cover" };
+  //   break;
       
                        }
   
