cf6b161f561945992818933b294cd479b3897fb1
suyunbae <suybae@ucdavis.edu>
2017-07-03 23:49:11 -0700

    everything but step 3

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,8 @@
 
 angular.module('weatherApp.controllers', [])
 
-
 .controller('weatherCtrl', function($scope, $http) {
   var vm = $scope;
-
-
 
   $http.get("http://ip-api.com/json").success(function(data) {
 
@@ -32,10 +29,135 @@ angular.module('weatherApp.controllers', [])
     var counter = 0;
 
   $scope.clickCounter =function () {
-      cityCounting = counter++;
+    cityCounting = counter++;
+    console.log(cityCounting);
+    var apiKey = "d544ef9e2e6f8bd6a4b1d6325950b77e";
+    var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + categories[cityCounting].city + ",us" + "&appid=" + apiKey;
+
+    $http.get(openWeatherURL).success(function(data) {
+      console.log("why are you here?");
       console.log(cityCounting);
-      return cityCounting;
-    }
+      vm.description[1] = data.weather[0].description;
+      vm.speed = (2.237 * data.wind.speed).toFixed(1) + " mph";
+      vm.name[cityCounting] = data.name;
+      console.log("get by forecast");
+      vm.humidity = data.main.humidity + " %";
+      vm.temp = data.main.temp;
+      vm.fTemp = (vm.temp * (9 / 5) - 459.67).toFixed(1) + " °F";
+      vm.cTemp = (vm.temp - 273).toFixed(1) + " °C";
+      vm.icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+      //Getting the weather icon
+      if (data.weather[0].id >= 200 && data.weather[0].id < 300) {
+        $scope.weatherClass = "wi wi-thunderstorm";
+      }
+
+      if (data.weather[0].id >= 300 && data.weather[0].id < 400) {
+        $scope.weatherClass = "wi wi-sprinkle";
+      }
+
+      if (data.weather[0].id >= 500 && data.weather[0].id < 600) {
+        if (data.weather[0].id == 500 || data.weather[0].id >= 520) {
+          $scope.weatherClass = "wi wi-rain";
+        }
+        $scope.weatherClass = "wi wi-showers";
+      }
+
+      if (data.weather[0].id >= 600 && data.weather[0].id < 700) {
+        $scope.weatherClass = "wi wi-snow";
+      }
+
+      if (data.weather[0].id >= 700 && data.weather[0].id < 800) {
+        $scope.weatherClass = "wi wi-fog";
+      }
+
+      if (data.weather[0].id == 800) {
+        $scope.weatherClass = "wi wi-day-sunny";
+      }
+
+      if (data.weather[0].id == 801) {
+        $scope.weatherClass = "wi wi-day-sunny-overcast";
+      }
+
+      if (data.weather[0].id == 802) {
+        $scope.weatherClass = "wi wi-day-cloudy";
+      }
+
+      if (data.weather[0].id == 803 || data.weather[0].id == 804) {
+        $scope.weatherClass = "wi wi-cloudy";
+      }
+
+      if (data.weather[0].id == 900) {
+        $scope.weatherClass = "wi wi-tornado";
+      }
+
+      if (data.weather[0].id == 901 || data.weather[0].id == 960 || data.weather[0].id == 961) {
+        $scope.weatherClass = "wi wi-thunderstorm";
+      }
+
+      if (data.weather[0].id == 902 || data.weather[0].id == 962) {
+        $scope.weatherClass = "wi wi-hurricane";
+      }
+
+      if (data.weather[0].id == 903) {
+        $scope.weatherClass = "wi wi-snowflake-cold";
+      }
+
+      if (data.weather[0].id == 904) {
+        $scope.weatherClass = "wi wi-hot";
+      }
+
+      if (data.weather[0].id == 905) {
+        $scope.weatherClass = "wi wi-strong-wind";
+      }
+
+      if (data.weather[0].id == 906) {
+        $scope.weatherClass = "wi wi-hail";
+      }
+
+      if (data.weather[0].id == 951) {
+        $scope.weatherClass = "wi wi-day-sunny";
+      }
+
+      if (data.weather[0].id >= 952 && data.weather[0].id <= 956) {
+        $scope.weatherClass = "wi wi-windy";
+      }
+
+      if (data.weather[0].id >= 957 && data.weather[0].id <= 959) {
+        $scope.weatherClass = "wi wi-strong-wind";
+      }
+
+
+      // Calculate current hour using offset from UTC.
+
+      var a = new Date(data.dt * 1000);
+      var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+
+      // Hours part from the timestamp
+      var hours = a.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + a.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + a.getSeconds();
+
+      vm.formattedDate = date + ' ' + month + ' ' + year;
+      vm.formattedTime = hours + ':' + minutes.substr(-2);
+
+
+      //Formatted time for sunrise
+      var sunrise = new Date(data.sys.sunrise*1000);
+      var sunriseHours = sunrise.getHours();
+      var sunriseMinutes = "0" + sunrise.getMinutes();
+    
+      vm.formattedSunrise = sunriseHours + ':' + sunriseMinutes.substr(-2) + ' AM';
+      
+
+    }); //closing OpenWeatherMap
+
+    } //closing clickCounter
     console.log(cityCounting);
     if(cityCounting == 0){
       var apiKey = "d544ef9e2e6f8bd6a4b1d6325950b77e";
@@ -43,6 +165,7 @@ angular.module('weatherApp.controllers', [])
     console.log("no im from city original here");
     }
     else{
+
       var apiKey = "d544ef9e2e6f8bd6a4b1d6325950b77e";
     var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityCounting + ",us" + "&appid=" + apiKey;
         console.log("no im from city counting");
--- js/directives.js
+++ js/directives.js
@@ -20,7 +20,19 @@ app.directive('addbuttons', function($compile){
   return function(scope, element, attrs){
     element.bind("click", function(){
       scope.count++;
-      angular.element(document.getElementById('space-for-buttons')).prepend($compile("<div class= panel ><h3 id=title>{{name | uppercase}}</h3><br><i ng-class=weatherClass id=icon></i><h3 id= temp >{{fTemp | uppercase}}</h3><p style= 'color: #FAFAFA;' >{{description | uppercase}}</p><p>{{location}}</p><br><div id= wrapper ><div id= first ><i id= smallIcons  class= 'wi wi-horizon-alt' ></i><p id= eventID >SUNRISE</p><p id= subID >{{formattedSunrise}}</p></div><div id= second ><i class=  'wi wi-strong-wind'  id= smallIcons ></i><p id= eventID >WIND</p><p id= subID >{{speed}}</p></div><div id= third ><i class=  'wi wi-humidity'  id= smallIcons ></i><p id= eventID >HUMIDITY</p><p id= subID >{{humidity}}</p></div></div> <!--End of Wrapper--></div> <!--End of Panel-->")(scope));
+      angular.element(document.getElementById('space-for-buttons')).prepend($compile(
+        "<div class= panel>"+
+          "<h3 id=title>{{name[" + "1" +"]| uppercase}}</h3><br>" +
+          "<i ng-class=weatherClass id=icon></i>" +
+          "<h3 id= temp >{{fTemp | uppercase}}</h3>" +
+          "<p style= 'color: #FAFAFA;' >{{description| uppercase}}</p>"+
+          "<p>{{location}}</p><br>"+
+        "<div id= wrapper ><div id= first ><i id= smallIcons  class= 'wi wi-horizon-alt' ></i>" +
+          "<p id= eventID >SUNRISE</p><p id= subID >{{formattedSunrise}}</p></div>"+
+        "<div id= second ><i class=  'wi wi-strong-wind'  id= smallIcons ></i>"+
+          "<p id= eventID >WIND</p><p id= subID >{{speed}}</p></div>"+
+        "<div id= third ><i class=  'wi wi-humidity'  id= smallIcons ></i>"+
+          "<p id= eventID >HUMIDITY</p><p id= subID >{{humidity}}</p></div></div></div>")(scope));
     });
   };
 });
